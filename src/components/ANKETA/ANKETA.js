adc69bc7d458bce6052d8aea4c35fa662deed7a5
MaksimNevzorov <nevzorov.work@yandex.by>
2022-09-26 19:52:31 +0300

    corrections code ANKETA

--- src/components/ANKETA/ANKETA.js
+++ src/components/ANKETA/ANKETA.js
@@ -1,46 +1,41 @@
-export const ANKETA = ()=>{
+export const ANKETA = () => {
+  let lastName, firstName, middleName, age, sex, pension;
 
-    let lastName,
-    firstName,
-    middleName,
-    age,
-    sex,
-    pension;
+  do {
+    lastName = prompt("Ваша фамилия?", "");
+  } while (lastName.length < 1);
 
-do {
-    lastName = prompt('Ваша фамилия?', '');
-} while (lastName.length < 1 || lastName.typeof === Number);
+  do {
+    firstName = prompt("Ваше имя?", "");
+  } while (firstName.length < 1);
 
-do {
-    firstName = prompt('Ваше имя?', '');
-} while (firstName.length < 1 || firstName.typeof === Number);
+  do {
+    middleName = prompt("Ваше отчество?", "");
+  } while (middleName.length < 1);
 
-do {
-    middleName = prompt('Ваше отчество?', '');
-} while (middleName.length < 1 || middleName.typeof === Number);
+  do {
+    age = Number(prompt("Сколько Вам полных лет?", ""));
+  } while (age == "");
 
-do {
-    age = +prompt('Сколько Вам полных лет?', '');
-} while (age ==  '');
+  if (confirm("Вы мужчина?", "") == true) {
+    sex = "Мужской";
+  } else {
+    sex = "Женский";
+  }
 
+  if (age < 60) {
+    pension = "Нет";
+  } else {
+    pension = "Да";
+  }
 
- if (confirm('Вы мужчина?', '') == true){
-    sex = 'Мужской'
-} else { 
-    sex = 'Женский'
-} ;
-
-if (age < 60){
-    pension = 'Нет'
-} else {
-    pension = 'Да'
-}
-
-
-alert(`Ваше ФИО: ${lastName} ${firstName} ${middleName}\n 
+  alert(
+    `Ваше ФИО: ${lastName} ${firstName} ${middleName}\n 
 Вам полных лет: ${age}\n 
-Ваш пол:  ${sex}` + 
-`\nЧерез 5 лет Вам будет` + ' ' +(age + 5) +
-`\nПенсия: ${pension}`);
-
-}
+Ваш пол:  ${sex}` +
+      `\nЧерез 5 лет Вам будет` +
+      " " +
+      (age + 5) +
+      `\nПенсия: ${pension}`
+  );
+};
